ce9eb08e375387766a4a2c82ba2e6eccedf808cb
mateusz <mateusz@drankowski.com.pl>
2017-09-11 21:29:00 +0100

    [mysql] Increase the size of type / agent columns

--- mysql/sql/db_create.sql
+++ mysql/sql/db_create.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS `logs` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `host` VARCHAR(60),
   `clientip` VARCHAR(20),
-  `type` VARCHAR(6),
+  `type` VARCHAR(20),
   `request` VARCHAR(60),
   `response` INT(4),
   `referrer` VARCHAR(60),
-  `agent` VARCHAR(60),
+  `agent` VARCHAR(240),
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`id`)
 ) DEFAULT CHARSET=utf8 COMMENT='Access logs'
